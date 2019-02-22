c83870c09cd41f881b1adaf96618d41d69a5baae
Jin Lee <wisedev43@gmail.com>
2019-02-22 10:38:43 -0800

    Setuo separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +11,7 @@ var {User} = require('./models/user');
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
